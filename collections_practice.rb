51fe094c0cc4b83a5f1eb311bad41fe24fbd30f8
Ryna Truong <rynatruong@outlook.com>
2020-02-23 17:28:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(arr_int)
+  arr_int.sort 
+end
 
+def sort_array_desc(arr)
+  desc_arr = arr.sort
+  desc_arr.reverse
+end
+
+def sort_array_char_count(arr_str)
+  arr_str.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.sort do |b, c|
+  b[2] <=> c[1]
+  end
+end
+
+def reverse_array(ints)
+  ints.reverse
+end
+
+def kesha_maker(kesha_array)
+  new_arr = []
+  kesha_array.each do |letter|
+    new_arr << letter[2] = "$"
+  end
+  new_arr
+end
+    
